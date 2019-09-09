752b996dd1d14ba66dee8f99f439e69e76ce5bb6
Artem Kuzmenko <arkuzka@mail.ru>
2019-09-09 10:41:58 +0300

    renamed actions and added checks for get_datetime_comand_handler

--- actions.py
+++ actions.py
@@ -1,16 +1,16 @@
 import logging
-import requests as req
+import requests
 import datetime
 
 
-def start(bot, update):
+def start_comand_handler(bot, update):
     """ Функция отправляет ответ на команду /start. """
     text = 'Вызвана команда /start'
     logging.info(text)
     update.message.reply_text(text)
 
 
-def talk_to_me(bot, update):
+def send_mirror_message_handler(bot, update):
     """ Функция отправляет пользователю его же сообщение. """
     user_text = 'Привет, {first_name}. Ты написал: {text}'.format(
         first_name=update.message.chat.first_name,
@@ -24,17 +24,23 @@ def talk_to_me(bot, update):
     update.message.reply_text(user_text)
 
 
-def now(bot, update):
+def get_datetime_comand_handler(bot, update):
     """ Функция отправляет пользователю время и дату в Москве. """
-    text = 'Вызвана команда /now'
-    ts = req.get('https://time100.ru/api.php').text
-    value = datetime.datetime.fromtimestamp(int(ts))
-    cur_date = value.strftime('%d.%m.%Y')
-    cur_time = value.strftime('%H:%M:%S')
-    msg_text = 'Сейчас в Москве:\n     Время: {time}\n     Дата: {date}'.format(
-        time=cur_time,
-        date=cur_date
-    )
-    logging.info('{} - {}'.format(text, msg_text))
-    update.message.reply_text(msg_text)
+    try:
+        resp = requests.get('https://time100.ru/api.php')
+        if resp.status_code == 200:
+            text = 'Вызвана команда /now'
+            value = datetime.datetime.fromtimestamp(int(resp.text))
+            cur_date = value.strftime('%d.%m.%Y')
+            cur_time = value.strftime('%H:%M:%S')
+            msg_text = 'Сейчас в Москве:\n     Время: {time}\n     Дата: {date}'.format(
+                time=cur_time,
+                date=cur_date
+            )
+            logging.info('{0} - {1}'.format(text, msg_text))
+            update.message.reply_text(msg_text)
+        else:
+            logging.error('Ошибка. Код ответа %s', resp.status_code)
+    except Exception:
+        logging.error('Ошибка - %s', Exception.message)
 
--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import config as conf
 import actions as act
@@ -12,13 +13,23 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     )
 
 
+# Формирование словаря PROXY из переменных окружения
+PROXY = {
+    'proxy_url': os.environ['URL'],
+    'urllib3_proxy_kwargs': {
+        'username': os.environ['USERNAME'],
+        'password': os.environ['PASSWORD'],
+    }
+}
+
+
 def main():
-    mybot = Updater(conf.API_KEY, request_kwargs=conf.PROXY)
+    mybot = Updater(os.environ['API_KEY'], request_kwargs=PROXY)
     logging.info('Стартует БОТ')
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler('start', act.start))
-    dp.add_handler(CommandHandler('now', act.now))
-    dp.add_handler(MessageHandler(Filters.text, act.talk_to_me))
+    dp.add_handler(CommandHandler('start', act.start_comand_handler))
+    dp.add_handler(CommandHandler('now', act.get_datetime_comand_handler))
+    dp.add_handler(MessageHandler(Filters.text, act.send_mirror_message_handler))
     mybot.start_polling()
     mybot.idle()
 
