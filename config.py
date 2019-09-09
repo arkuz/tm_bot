ecc3224510adabb684f7634f8f5a7bd346c79cb1
Artem Kuzmenko <arkuzka@mail.ru>
2019-09-09 10:40:02 +0300

    changed to os.environ

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
-# ключ API для бота
-API_KEY = '690634495:AAG0XH4udjyu72MzibFfoUpmTFTGGQwdvdo'
+import os
 
+
+# ключ API для бота
+os.environ['API_KEY'] = ''
 
 # Настройки прокси
-PROXY = {
-    'proxy_url': 'socks5://t2.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
-}
+os.environ['URL'] = 'socks5://'
+os.environ['USERNAME'] = ''
+os.environ['PASSWORD'] = ''
 
