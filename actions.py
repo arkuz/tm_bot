637d1491b044063224c53509e5389f9ef5f8e725
Artem Kuzmenko <arkuzka@mail.ru>
2019-09-10 13:25:47 +0300

    refactored weather_comand_handler

--- actions.py
+++ actions.py
@@ -1,7 +1,6 @@
 import os
 import logging
 import requests
-import config
 
 
 def start_comand_handler(bot, update):
@@ -25,31 +24,23 @@ def send_mirror_message_handler(bot, update):
     update.message.reply_text(user_text)
 
 
-def weather_comand_handler(bot, update):
-    """ Функция отправляет температуру в указанном городе """
+def weather_comand_handler(bot, update, args):
+    """ Функция отправляет температуру в указанном городе. """
     text = update.message.text
-    length_cmd = update.message.entities[0].length  # узнаем длину сообщения
-    city = text[length_cmd + 1:].replace(' ','')    # забираем все после /weather и удаляем пробелы
-    params = {
-        'q': city,
-        'APPID': os.environ['WEATHER_API_KEY'],
-    }
-    url = 'http://api.openweathermap.org/data/2.5/weather'
+    resp = _get_weather(args[0])
 
-    try:
-        resp = requests.get(url, params=params)
-    except (requests.ConnectionError,
-            requests.ConnectTimeout,
-            ) as e:
-        logging.error('Ошибка {0}'.format(e.message))
-        return  # выходим из функции если ошибка соединения
+    if resp is None:
+        return
+
+    if resp.status_code in [200, 404]:
+        resp_json_dict = resp.json()
 
     if resp.status_code == 200:
-        kelvin = resp.json()['main']['temp']
+        kelvin = resp_json_dict['main']['temp']
         degree = round(kelvin - 273)
         msg_text = 'Сейчас {0} °C'.format(degree)
         logging.info('{0} - {1}'.format(text, msg_text))
-    elif resp.status_code == 404 and resp.json()['message'] == 'city not found':
+    elif resp.status_code == 404 and resp_json_dict['message'] == 'city not found':
         msg_text = 'Я не знаю такого города.'
         logging.info('{0} - {1}'.format(text, msg_text))
     elif resp.status_code == 400:
@@ -59,4 +50,19 @@ def weather_comand_handler(bot, update):
         logging.error('Ошибка. Сервер вернул код {0}'.format(resp.status_code))
 
     update.message.reply_text(msg_text)
+
+
+def _get_weather(city):
+    params = {
+        'q': city,
+        'APPID': os.environ['WEATHER_API_KEY'],
+    }
+    url = 'http://api.openweathermap.org/data/2.5/weather'
+    try:
+        return requests.get(url, params=params)
+    except (requests.ConnectionError,
+            requests.ConnectTimeout,
+            ) as e:
+        logging.error('Ошибка {0}'.format(e.message))
+        return None
 
