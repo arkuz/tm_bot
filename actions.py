731f91afa7a121b28e0d8bf8fda26c046ae93f7a
Artem Kuzmenko <arkuzka@mail.ru>
2019-09-10 20:56:24 +0300

    refactored weather_comand_handler function

--- actions.py
+++ actions.py
@@ -26,42 +26,9 @@ def send_mirror_message_handler(bot, update):
 def weather_comand_handler(bot, update, args):
     """ Функция отправляет температуру в указанном городе. """
     text = update.message.text
-    resp = _get_weather(args[0])
-
+    resp = tools.get_weather(args[0])
     if resp is None:
         return
-
-    if resp.status_code in [200, 404]:
-        resp_json_dict = resp.json()
-
-    if resp.status_code == 200:
-        kelvin = resp_json_dict['main']['temp']
-        degree = round(kelvin - 273)
-        msg_text = 'Сейчас {0} °C'.format(degree)
-        logging.info('{0} - {1}'.format(text, msg_text))
-    elif resp.status_code == 404 and resp_json_dict['message'] == 'city not found':
-        msg_text = 'Я не знаю такого города.'
-        logging.info('{0} - {1}'.format(text, msg_text))
-    elif resp.status_code == 400:
-        msg_text = 'Нужно указать город. Например /weather Moscow'
-        logging.info('{0} - {1}'.format(text, msg_text))
-    else:
-        logging.error('Ошибка. Сервер вернул код {0}'.format(resp.status_code))
-
+    msg_text = tools.process_owm_response(text, resp)
     update.message.reply_text(msg_text)
-
-
-def _get_weather(city):
-    params = {
-        'q': city,
-        'APPID': os.environ['WEATHER_API_KEY'],
-    }
-    url = 'http://api.openweathermap.org/data/2.5/weather'
-    try:
-        return requests.get(url, params=params)
-    except (requests.ConnectionError,
-            requests.ConnectTimeout,
-            ) as e:
-        logging.error('Ошибка {0}'.format(e.message))
-        return None
 
