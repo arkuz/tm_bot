c34c83172ca963f72fcd21a2bdf7e5fdc40b9964
Artem Kuzmenko <arkuzka@mail.ru>
2019-09-14 16:59:12 +0300

    extracted logging ans send message in new func

--- actions.py
+++ actions.py
@@ -1,22 +1,11 @@
-import logging
 import owm_helpers
-
-
-LOG_STRING = 'Пользователь: {user}, Чат ID: {chat_id}, Сообщение: {msg}, Ответ: {answ}'
+from messages_helpers import send_text_to_user
 
 
 def start_comand_handler(bot, update):
     """ Функция отправляет ответ на команду /start. """
     answ_text = 'Вот такой командой можно узнать погоду: /weather Москва'
-
-    logging.info(LOG_STRING.format(
-        user=update.message.chat.username,
-        chat_id=update.message.chat.id,
-        msg=update.message.text,
-        answ=answ_text,
-    ))
-
-    update.message.reply_text(answ_text)
+    send_text_to_user(update, answ_text)
 
 
 def send_mirror_message_handler(bot, update):
@@ -25,57 +14,24 @@ def send_mirror_message_handler(bot, update):
         first_name=update.message.chat.first_name,
         msg=update.message.text,
     )
-
-    logging.info(LOG_STRING.format(
-        user=update.message.chat.username,
-        chat_id=update.message.chat.id,
-        msg=update.message.text,
-        answ=answ_text,
-    ))
-
-    update.message.reply_text(answ_text)
+    send_text_to_user(update, answ_text)
 
 
 def weather_comand_handler(bot, update, args):
     """ Функция отправляет температуру в указанном городе. """
-    city = ''
-    if args:
-        city = args[0]
+    city = args[0] if args else None
 
-    user = update.message.chat.username
-    chat_id = update.message.chat.id
-    msg = update.message.text
-
-    if not city:
+    if city is None:
         answ_text = 'Необходимо указать город. Пример: /weather Москва'
-        logging.info(LOG_STRING.format(
-            user=user,
-            chat_id=chat_id,
-            msg=msg,
-            answ=answ_text,
-        ))
-        update.message.reply_text(answ_text)
+        send_text_to_user(update, answ_text)
         return
 
-    try:
-        resp = owm_helpers.get_weather(city)
-    except (resp.ConnectionError,
-            resp.ConnectTimeout,
-            ) as e:
-        logging.error(LOG_STRING.format(
-            user=user,
-            chat_id=chat_id,
-            msg=msg,
-            answ=e,
-        ))
-        update.message.reply_text('Ошибка соединения с сервером openweathermap.org')
-        return
+    resp = owm_helpers.get_weather(city)
 
-    answ_text = owm_helpers.process_owm_response(resp)
-    logging.info(LOG_STRING.format(
-        user=user,
-        chat_id=chat_id,
-        msg=msg,
-        answ=answ_text,
-    ))
-    update.message.reply_text(answ_text)
+    if resp is not None:
+        answ_text = owm_helpers.process_owm_response(resp)
+        send_text_to_user(update, answ_text)
+
+    else:
+        answ_text = 'Ошибка соединения с сервером openweathermap.org'
+        send_text_to_user(update, answ_text)
