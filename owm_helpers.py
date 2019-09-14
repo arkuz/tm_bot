23e85ce62321382024a09ed5d44f5d910f2df55e
Artem Kuzmenko <arkuzka@mail.ru>
2019-09-14 17:01:15 +0300

    added changes for get weather mechanism

--- owm_helpers.py
+++ owm_helpers.py
@@ -1,15 +1,23 @@
 import os
 import requests
+import logging
 
 
 def get_weather(city):
     """ Функция для получения ответа от сервиса openweathermap.org. """
     params = {
         'q': city,
+        'units': 'metric',
         'APPID': os.environ['WEATHER_API_KEY'],
     }
     url = 'http://api.openweathermap.org/data/2.5/weather'
-    return requests.get(url, params=params)
+    try:
+        resp = requests.get(url, params=params)
+        return resp
+    except (requests.exceptions.ConnectionError,
+            requests.exceptions.ConnectTimeout) as e:
+        logging.error(e)
+        return None
 
 
 def process_owm_response(response):
@@ -18,9 +26,7 @@ def process_owm_response(response):
         resp_json_dict = response.json()
 
     if response:
-        kelvin = resp_json_dict['main']['temp']
-        degree = round(kelvin - 273)
-        answ_text = 'Сейчас {0} °C'.format(degree)
+        answ_text = 'Сейчас {0} °C'.format(resp_json_dict['main']['temp'])
 
     elif response.status_code == 404 and resp_json_dict['message'] == 'city not found':
         answ_text = 'Я не знаю такого города.'
